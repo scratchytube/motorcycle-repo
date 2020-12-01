c9e20c8f42b51c457b4bdb5cbcfa26342d72533f
Brian Sahota <bpsahota@gmail.com>
2020-11-30 20:12:44 -0500

    fetch is working

--- src/index.js
+++ src/index.js
@@ -26,9 +26,10 @@
 
 /****FETCH***/
 
-const getMotorcycles () => {
+const getMotorcycles = () => {
     fetch('http://localhost:3000/api/v1/motorcycles')
     .then(r => r.json())
     .then(console.log)
 }
+getMotorcycles()
 
