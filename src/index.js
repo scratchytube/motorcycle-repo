30d58e286f59b0dd2feabf671e37e7b539e9c2f1
techtwins <gshevaughn@gmail.com>
2020-12-05 14:09:46 -0500

    delete and update for the appointment forms are both working

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ const accountForm = document.querySelector("#account-form")
 const modalSubmitBtn = document.querySelector("#submit-btn2")
 const acctBtn = document.querySelector("#acct-btn")
 const appointments = document.querySelector("#appointment-div")
-const apptDate = document.querySelector("#my-appt-date")
 const updateApptBtn = document.querySelector("#update-appt")
 const closeBtn = document.querySelector(".close-button")
 const companyField = document.querySelector("#cycle-input")
 const apptsContainer = document.querySelector('#user-appts-div')
-
+const lookbookDiv = document.querySelector("#lookbook-div")
 
 const renderAllMotorcycles = cycleObj => {
   cycleObj.forEach(bike => {
@@ -27,20 +26,34 @@ const renderAllMotorcycles = cycleObj => {
 }
 
 
-const renderAppointments = (apptObj) => {
+const renderAppointments = apptObj => {
   apptObj.forEach(appt => {
     const myApptForm = document.createElement('form')
+    myApptForm.className = 'myappts'
     myApptForm.dataset.id = appt.id
     myApptForm.innerHTML = `
       <br><br><br>
-      <span dataset-id = ${appt.id} class="close-button">x</span>
+      <span data-id=${appt.id} class="close-button">x</span>
       <label for="">Date of appointment: </label>
-      <input id="my-appt-date" type="text" placeholder="${appt.day} ${appt.time}">
+      <input id="my-appt-day" type="text" placeholder="${appt.day}">
+      <input id="my-appt-time" type="text" placeholder="${appt.time}">
       <label for="">Company: </label>
-      <input id="cycle-input" type="text" placeholder="Cycle Heaven"><br>
-      <button id="update-appt">Update Appointment</button>
+      <input id="cycle-input" type="text" placeholder="Cycle Heaven" disabled><br>
+      <button data-action="update" id="update-appt">Update Appointment</button>
     `
     apptsContainer.append(myApptForm)
+  })
+}
+
+const renderLookbooks = lookbookObj => {
+  lookbookObj.forEach(lookbook => {
+    const img = document.createElement('img')
+    img.dataset.id = lookbook.id
+    img.className = 'lookbook-img'
+    img.src = lookbook.imgUrl
+    img.alt = `${lookbook.name}`
+
+    lookbookDiv.append(img)
   })
 }
 
@@ -54,7 +67,7 @@ const appointmentSubmission = event => {
     day: form.querySelector('#day').value,
     time: form.querySelector('#time-input').value,
     // increment motorcycle_id + 1 and stop when it hits 10
-    motorcycle_id: 4
+    motorcycle_id: 21 // ids now start at 21..
     // motorcycle_id: form.querySelector('#make').value
   }
   // console.log(modObj)
@@ -86,26 +99,58 @@ const accountSubmit = event => {
   // console.log(username)
   // username.value = document.querySelector("#acct-text")
   acctBtn.textContent = `Hi ${username}!`
+
+  const modalClass = document.querySelector('.modal-content')
+  // const closeBtn = modalCLass.querySelector('.span')
+  //closeBtn.addEventListener('')
+
   
   event.target.reset()
 }
 
 // DELETE FUNCTION HERE //
 const deleteAppt = event => {
-  console.log(event.target)
+  
+  if (event.target.matches('.close-button')) {
+    
+    const btn = event.target
+    const form = btn.closest('.myappts')
+    const id = form.dataset.id
 
-  id = event.target.dataset.id
-  console.log(id)
-
-  const form = event.target.closest('form')
-
-  if (event.target.className === 'close-button') {
-    form.remove()
-  }
-
-  fetch(`http://localhost:3000/api/v1/modification_requests/${id}`, {
+    fetch(`http://localhost:3000/api/v1/modification_requests/${id}`, {
     method: 'DELETE',
-  })
+    })
+    .then(r => r.json())
+    .then(data => {
+      console.log('Success: ', data)
+    })
+
+    form.remove()
+  } else if (event.target.dataset.action === 'update') {
+
+    const btn = event.target
+    const form = btn.closest('.myappts')
+    const id = form.dataset.id
+    const apptDay = form.querySelector("#my-appt-day");
+    const apptTime = form.querySelector("#my-appt-time");
+
+    const apptDateObj = {
+      day: apptDay.value,
+      time: apptTime.value
+    }
+    console.log(apptDateObj)
+    // debugger
+
+    fetch(`http://localhost:3000/api/v1/modification_requests/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(apptDateObj)
+    })
+    .then(r => r.json())
+    .then(console.log)
+  }
 }
 
 /****EVENTS***/
@@ -135,7 +180,17 @@ const getAppointments = () => {
     })
 }
 
+const getLookbook = () => {
+  fetch("http://localhost:3000/api/v1/lookbooks")
+  .then(r => r.json())
+    .then(data => {
+      console.log(data)
+      renderLookbooks(data)
+  })
+}
+
+// INITIALIZE 
 getMotorcycles()
 getAppointments()
-// getLookbook()
+getLookbook()
 
